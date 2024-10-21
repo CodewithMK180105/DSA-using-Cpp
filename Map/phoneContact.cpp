4a019ab1ace37c7e42df60ce150c5f3c3d2454d3
CodewithMK180105 <manishkumar180105@gmail.com>
2024-10-21 22:00:57 +0530

    Map inbuilt Functions

---
+++ Map/phoneContact.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<map>
+#include<algorithm>
+using namespace std;
+int main(){
+    map<string,int> call;
+    call["John"] = 1234;
+    call["Gane"] = 9876;
+    call.insert(make_pair("Ram",4563));
+
+    for(auto element: call){
+        cout<<element.first<<"=>"<<element.second<<endl;
+    }cout<<endl;
+
+    call["Gane"] = 5845;
+    for(auto element: call){
+        cout<<element.first<<"=>"<<element.second<<endl;
+    }cout<<endl;
+
+    return 0;
+}
