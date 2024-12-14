723ef6f47497d92a60e5b5c0586db5cd084d8d44
CodewithMK180105 <manishkumar180105@gmail.com>
2024-12-14 17:25:17 +0530

    Count no. of unnecessary palindrome and Postfix Evaluation

---
+++ stacks/challeng17.cpp
@@ -0,0 +1,53 @@
+// Evaluate the postfix operation.
+
+#include<iostream>
+#include<stack>
+#include<string>
+using namespace std;
+int calc(int m,int n,char operand){
+    switch(operand){
+        case '+': return n+m;
+        case '-': return n-m;
+        case '*': return n*m;
+        case '/': return n/m;
+        case '%': return n%m;
+    }
+}
+int evaluatePostfix(string s){
+    int n=s.size();
+    stack<int> st;
+
+    for(int i=0;i<n;i++){
+        if('*'==s[i] || '/'==s[i] || '%'==s[i] || '+'==s[i] || '-'==s[i]){
+            int a,b;
+            if(!st.empty()){
+                a=st.top();
+                st.pop();
+            }
+            if(!st.empty()){
+                b=st.top();
+                st.pop();
+            }
+            else{
+                cout<<"Invalid expression"<<endl;
+                return 0;
+            }
+            int res=calc(a,b,s[i]);
+            st.push(res);
+        } else{
+            st.push(s[i]-'0');
+        }
+    }
+    int ans=st.top();
+    st.pop();
+    if(!st.empty()){
+        cout<<"Invalid Expression"<<endl;
+        return 0;
+    }
+    return ans;
+}
+int main(){
+    string s="231*+9-";
+    cout<<evaluatePostfix(s);
+    return 0;
+}
--- stacks/challenge16.cpp
+++ stacks/challenge16.cpp
@@ -1,15 +1,27 @@
 // Find the minimum number of brackets that we need to remove to make the given bracket sequence balanced.
+// Note: s contains only rounded paranthesis =>either '(' or ')'
 
 #include<iostream>
 #include<stack>
 using namespace std;
-bool removeInvalidParentheses(string s){
+int removeInvalidParentheses(string s){
     int n=s.length();
     stack<char> st;
+    int count=0;
 
-    
+    for(int i=0;i<n;i++){
+        if(')'==s[i] && st.empty()) count++;
+        else if(')'==s[i] && !st.empty()) st.pop();
+        else st.push(s[i]);
+    }
+    while(!st.empty()){
+        count++;
+        st.pop();
+    }
+    return count;
 }
 int main(){
-
+    string s="(())())))";
+    cout<<removeInvalidParentheses(s);
     return 0;
 }
